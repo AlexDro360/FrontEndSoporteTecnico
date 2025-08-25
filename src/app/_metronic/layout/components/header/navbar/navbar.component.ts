f7f9d34f4ea543bf15718d734ccaa7dd5b9f47c6
JazminMartinezC <21160705@itoaxaca.edu.mx>
2025-08-25 16:15:19 -0600

    cambios

--- src/app/_metronic/layout/components/header/navbar/navbar.component.ts
+++ src/app/_metronic/layout/components/header/navbar/navbar.component.ts
@@ -31,11 +31,7 @@ export class NavbarComponent implements OnInit {
     if (userString) {
       try {
         const user = JSON.parse(userString);
-        this.avatar = user.avatar.replace(
-          "http://10.168.0.108:8001/storage",
-          "http://10.168.0.108:8000/storage"
-        );
-        console.log(this.avatar);
+        this.avatar = user.avatar;
         this.cd.detectChanges();
       } catch (e) {
         console.error('Error parsing user from localStorage', e);
